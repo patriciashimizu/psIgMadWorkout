83c47c3518fea044ac04b94a3295cdfbdb4d0b71
eleves <patriciashimizu@hotmail.com>
2017-08-17 17:20:38 -0400

    MARK - ViewController

--- psIgMadWorkout/ViewController.swift
+++ psIgMadWorkout/ViewController.swift
@@ -4,6 +4,7 @@ import WatchConnectivity //api watch
 // ===================================================
 class ViewController: UIViewController, WCSessionDelegate {
     // ------------------------------------
+    // MARK: ------ OUTLETS
     @IBOutlet weak var theDatePicker: UIDatePicker!
     @IBOutlet weak var thePickerView: UIPickerView!
     @IBOutlet weak var theScrollView: UIScrollView!
@@ -12,17 +13,18 @@ class ViewController: UIViewController, WCSessionDelegate {
     @IBOutlet weak var theSynchButton: UIButton!
     @IBOutlet weak var theRepsField: UITextField!
     @IBOutlet weak var theSetsField: UITextField!
-    
+    // ------------------------------------
+    // MARK: ------ PROPERTIES
     var exerciseAccount: UserDefaults = UserDefaults.standard
     var session: WCSession!
     var contReset = 0
     var contSaveClipboard = 0
-    // ------------------------------------
     var exerciseAccountability = ["HEART: Treadmill" : 0, "LEGS: Laying Leg Press" : 0, "HAMSTRINGS: Laying Hamstring Curl" : 0, "HAMSTRINGS: Seated Hamstring Curls" : 0, "CALVES: Calf Press" : 0, "CALVES: Seated Calf Raise" : 0, "QUADS: Leg Extension" : 0, "INNER THIGH: Adductor" : 0, "GLUTES: Abductor" : 0, "GLUTES: Glute Kickback" : 0, "CHEST: Chest Press" : 0, "CHEST: Plated Chess Press" : 0, "CHEST: Pec Tec" : 0, "BACK: Cable Low Rows" : 0, "BACK: Cable Nose Pulls" : 0, "CHEST: Cable Flyes" : 0, "LATS: Lateral Pull-Downs" : 0, "ABS: Ab Cruch Machine" : 0, "LEGS: Standing Leg Press" : 0, "BACK: Rear Delt Flyes" : 0, "CHEST: Inclined Chess Press" : 0, "CHEST: Dumbell Flyes" : 0, "BICEPS: Preacher Curl" : 0, "BICEPS: Independant Bicep Curl" : 0, "TRICEPS: Tricep Pull-Down" : 0, "BICEPS: Cable Row Bicep Curls" : 0, "TRICEPS: Cable Row Pull-Downs" : 0, "TRICEPS: Bar Pull-Downs" : 0, "BICEPS: Overhead Cable Curls" : 0, "TRICEPS: Assisted Dips" : 0, "LATS: Assisted Pull-Ups" : 0, "BACK: Bentover Dumbell Rows" : 0, "BICEPS: Dumbell Curls" : 0, "TRICEPS: Dumbell Kickbacks" : 0, "BICEPS: Barbell Curls" : 0, "TRICEPS: Skull Crushers" : 0, "TRICEPS: French Presses" : 0, "SHOULDERS: Arnold Presses" : 0, "SHOULDERS: Overhead Presses" : 0, "SHOULDERS: Hammer Flyes" : 0, "SHOULDERS: Cable Upward Rows" : 0, "SHOULDERS: Barbell Upward Rows" : 0, "SHOULDERS: Cable Lateral Raises" : 0, "SHOULDERS: Dumbell Lateral Raises" : 0, "DELTS: Dumbell Forward Raises" : 0, "DELTS: Cable Forward Raises" : 0]
     
     var theDatabase: [String : [[String : String]]]!
     var theExercise: String!
     //--------------------------------------------------
+    // MARK: ------ WCSessionDelegate FUNCTIONS
     @available(iOS 9.3, *)
     public func sessionDidDeactivate(_ session: WCSession) {
         //..
@@ -38,6 +40,7 @@ class ViewController: UIViewController, WCSessionDelegate {
         //..
     }
     // ------------------------------------
+    // MARK: ------ SYSTEM FUNCTIONS
     override func viewDidLoad() {
         super.viewDidLoad()
         if WCSession.isSupported()
@@ -61,8 +64,8 @@ class ViewController: UIViewController, WCSessionDelegate {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
-    
     // ------------------------------------
+    // MARK: ------ OTHER FUNCTIONS
     @IBAction func hideKeyboard(_ sender: UIButton)
     {
         self.view.endEditing(true)
@@ -156,6 +159,7 @@ class ViewController: UIViewController, WCSessionDelegate {
         return 1
     }
     // ------------------------------------
+    // MARK: ------ UIDatePicker FUNCTIONS
     func pickerView(_ pickerView: UIPickerView, viewForRow row: Int, forComponent component: Int, reusingView view: UIView?) -> UIView
     {
         let pickerLabel = UILabel()
@@ -201,6 +205,44 @@ class ViewController: UIViewController, WCSessionDelegate {
         self.theExercise = anArrayOfString[row]
     }
     // ------------------------------------
+    func sendMessage(aDict: [String : String]) {
+        let messageToSend = ["Message" : aDict]
+        
+        session.sendMessage(messageToSend, replyHandler: {(replyMessage) in
+            DispatchQueue.main.async(execute: { () -> Void in})
+            
+        }) { (error) in
+            print("error: \(error.localizedDescription)")
+        }
+    }
+    // ------------------------------------
+    fileprivate func saveUserDefaultIfNeeded()
+    {
+        //self.exerciseAccount.removeObjectForKey("exercises")
+        
+        if !self.checkForUserDefaultByName("exercises", andUserDefaultObject: self.exerciseAccount)
+        {
+            self.exerciseAccount.setValue(self.exerciseAccountability, forKey: "exercises")
+        }
+        else
+        {
+            self.exerciseAccountability = self.exerciseAccount.value(forKey: "exercises") as! [String : Int]
+        }
+    }
+    // ------------------------------------
+    func checkForUserDefaultByName(_ theName: String, andUserDefaultObject: UserDefaults) -> Bool
+    {
+        let userDefaultObject = andUserDefaultObject.object(forKey: theName)
+        
+        if userDefaultObject == nil
+        {
+            return false
+        }
+        
+        return true
+    }
+    // ------------------------------------
+    // MARK: ------ BUTTONS
     //ADD SET
     @IBAction func addSetButton(_ sender: UIButton) {
         self.addExercise()
@@ -243,32 +285,7 @@ class ViewController: UIViewController, WCSessionDelegate {
         sendMessage(aDict: dictToSendWatch)
     }
     // ------------------------------------
-    func sendMessage(aDict: [String : String]) {
-        let messageToSend = ["Message" : aDict]
-        
-        session.sendMessage(messageToSend, replyHandler: {(replyMessage) in
-            DispatchQueue.main.async(execute: { () -> Void in})
-            
-        }) { (error) in
-            print("error: \(error.localizedDescription)")
-        }
-    }
-    // ------------------------------------
-    fileprivate func saveUserDefaultIfNeeded()
-    {
-        //self.exerciseAccount.removeObjectForKey("exercises")
-        
-        if !self.checkForUserDefaultByName("exercises", andUserDefaultObject: self.exerciseAccount)
-        {
-            self.exerciseAccount.setValue(self.exerciseAccountability, forKey: "exercises")
-        }
-        else
-        {
-            self.exerciseAccountability = self.exerciseAccount.value(forKey: "exercises") as! [String : Int]
-        }
-    }
-    // ------------------------------------
-    // LOGO (saveToClipboard)
+    // SAVE TO CLIPBOARD
     @IBAction func saveToClipboard(_ sender: UIButton) {
         contSaveClipboard+=1
         
@@ -282,18 +299,7 @@ class ViewController: UIViewController, WCSessionDelegate {
         
     }
     // ------------------------------------
-    func checkForUserDefaultByName(_ theName: String, andUserDefaultObject: UserDefaults) -> Bool
-    {
-        let userDefaultObject = andUserDefaultObject.object(forKey: theName)
-        
-        if userDefaultObject == nil
-        {
-            return false
-        }
-        
-        return true
-    }
-    // ------------------------------------
+    // RESET
     @IBAction func resetExercises(_ sender: UIButton) {
         contReset += 1
         if contReset == 5 {
@@ -314,7 +320,7 @@ class ViewController: UIViewController, WCSessionDelegate {
             
             //NO
             alert.addAction(UIAlertAction(title: "No", style: .cancel, handler: { (action: UIAlertAction!) in
-                
+                self.contReset = 0
             }))
             present(alert, animated: true, completion: nil)
         }
@@ -322,6 +328,7 @@ class ViewController: UIViewController, WCSessionDelegate {
     // ------------------------------------
 }
 // ===================================================
+// MARK: ------ EXTENSIONS
 extension Date
 {
     init(dateString:String) {
